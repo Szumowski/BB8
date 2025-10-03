44f49e6cdb3ef1a0f3dc00a857dfcc06fc43f97e
Szumi <156961250+Szumowski@users.noreply.github.com>
2025-10-03 22:37:50 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h> 
 #include <Wire.h>
 #include <VL53L0X.h>
+#include <Adafruit_NeoPixel.h>
 
 #define IN1 5 
 #define IN2 18 
 #define IN3 16
 #define IN4 17 
 
+#define LED_PIN 4
+#define LED_COUNT 16
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
+
 VL53L0X sensor;
+
+void setColor(uint8_t r, uint8_t g, uint8_t b) {
+  for(int i = 0; i < LED_COUNT; i++) {
+    strip.setPixelColor(i, strip.Color(r, g, b));
+  }
+  strip.show();
+}
 
 void setup(){ 
 
@@ -32,7 +45,14 @@ if(!sensor.init()){
 
 sensor.setTimeout(500);
 
+strip.begin();
+
+strip.show();
+
+setColor(0, 0, 255); 
+
 Serial.println("System Gototwy!");
+
 } 
 
 void loop(){ 
@@ -51,11 +71,15 @@ if (distance <200){
     digitalWrite(IN3, LOW);
     digitalWrite(IN4, LOW);
 
+    setColor(0, 255, 0); 
+
 } else {
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     digitalWrite(IN3, HIGH);
     digitalWrite(IN4, LOW);
+
+    setColor(255, 0, 0); 
 }
 
 delay(100);
